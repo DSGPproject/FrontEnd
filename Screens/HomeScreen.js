f35d76c336d5858042d8de521d10e2f166a375d5
udanthaaa <damikaudantha@gmail.com>
2024-04-07 17:10:23 +0530

    Homescreen changed

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, Text, TouchableOpacity, StyleSheet, Image, Dimensions } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
@@ -7,20 +8,36 @@ const windowHeight = Dimensions.get('window').height;
 const HomeScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <View style={[styles.header, { backgroundColor: '#0bce83' }, { height: windowHeight * 0.3 }]}>
+      <View style={[styles.header, { backgroundColor: '#0bce83' }, { height: windowHeight * 0.25 }]}>
         <Image source={require('../assets/background.png')} style={styles.backgroundImage} />
         <Text style={styles.headerText}>Welcome to GreenGuard!</Text>
       </View>
-
-      <View style={styles.content}>
-        <View style={styles.box}>
-          <Text style={styles.boxText}>Take a photo or upload a photo</Text>
-        </View>
-        <TouchableOpacity
+        
+      <LinearGradient
+  colors={["#b0ffcb", "#eae7e7"]}
+  style={[styles.box, { marginTop: -(windowHeight * 0.1) }]}
+>
+  <View style={styles.topicContainer}>
+    <Text style={styles.boxTitle}>Diagnose a tea plant disease</Text>
+  </View>
+  <View style={styles.contentContainer}>
+    <Text style={styles.boxDescription}>Take a photo of the tea plant leaf or upload a photo to diagnose diseases
+    </Text>
+    <View style={styles.imageContainer}>
+      <Image
+        source={require("../assets/TeaPlant.png")}
+        style={styles.teaPlantImage}
+      />
+    </View>
+  </View>
+  <TouchableOpacity
           style={styles.button}
           onPress={() => navigation.navigate('ScanScreen')}>
           <Text style={styles.buttonText}>Start Scan</Text>
         </TouchableOpacity>
+</LinearGradient>
+      
+      <View style={styles.content}>
         <TouchableOpacity
           style={styles.chatBtn}
           onPress={() => navigation.navigate('ChatScreen')}>
@@ -37,6 +54,7 @@ const HomeScreen = ({ navigation }) => {
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: '#F7FCF7',
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
@@ -54,7 +72,7 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     color: '#fff',
     position: 'absolute',
-    top: '50%',
+    top: '30%',
     left: '5%',
   },
   backgroundImage: {
@@ -76,8 +94,7 @@ const styles = StyleSheet.create({
   },
   box: {
     width: '80%',
-    height: windowHeight * 0.2,
-    backgroundColor: '#F7FCF7',
+    height: windowHeight * 0.25,
     alignItems: 'center',
     justifyContent: 'center',
     borderRadius: windowWidth * 0.03,
@@ -86,16 +103,46 @@ const styles = StyleSheet.create({
   boxText: {
     fontSize: windowWidth * 0.05,
   },
+  boxTitle: {
+    paddingLeft : '6%',
+    fontSize: windowWidth * 0.05,
+    fontWeight: 'bold',
+  },
+  topicContainer: {
+    marginBottom: windowHeight * 0.02,
+    alignSelf: 'flex-start',
+  },
+  contentContainer: {
+    paddingLeft : '6%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginBottom: windowHeight * 0.02,
+  },
+  boxDescription: {
+    fontSize: windowWidth * 0.036,
+    flex: 1,
+    marginRight: windowWidth * 0.02,
+  },
+  imageContainer: {
+    marginRight : '7%',
+    width: windowWidth * 0.2,
+    height: windowWidth * 0.2,
+  },
+  teaPlantImage: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover',
+  },
   vectorIcon: {
     width: windowWidth * 0.05,
     height: windowWidth * 0.05,
   },
   button: {
     backgroundColor: '#0bce83',
-    paddingVertical: windowHeight * 0.03,
-    paddingHorizontal: windowWidth * 0.1,
+    paddingVertical: windowHeight * 0.01,
+    paddingHorizontal: windowWidth * 0.05,
     borderRadius: windowWidth * 0.02,
-    marginBottom: windowHeight * 0.02,
   },
   buttonText: {
     color: '#fff',
